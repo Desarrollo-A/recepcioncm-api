127401d409a9b6a29c890983d81fa360697026ab
jcoronelg <programador.analista24@ciudadmaderas.com>
2022-10-26 13:42:59 -0500

    Se agrega título al reporte y estilos para la tabla

--- resources/views/pdf/reports/input-output-inventory.blade.php
+++ resources/views/pdf/reports/input-output-inventory.blade.php
@@ -1,5 +1,8 @@
 @extends('pdf.layout.app')
 @section('content')
+    <div class="title-report">
+        <h3>Reporte Entradas/Salidas Inventario</h3>
+    </div>
     <table class="content-table">
         <thead>
         <tr>
@@ -17,7 +20,7 @@
                 <td>{{$item->code}}</td>
                 <td>{{$item->name}}</td>
                 <td>{{$item->type}}</td>
-                <td>{{$item->sum_quantity}}</td>
+                <td>{{number_format($item->sum_quantity)}}</td>
                 <td>{{$item->sum_cost ? '$'.number_format($item->sum_cost) : 'No aplica'}}</td>
                 <td>{{$item->move_date->format('d-m-Y')}}</td>
             </tr>
