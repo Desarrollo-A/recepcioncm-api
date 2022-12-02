56dfec6a8dde109d20b44996f96170d3ddd0ca87
jcoronelg <programador.analista24@ciudadmaderas.com>
2022-12-02 09:10:19 -0600

    Migraciones de las tablas de solicitudes de chofer y auto * Se crean tablas base para la relación de horarios con las solicitudes de chofer y de auto

---
+++ database/migrations/2022_12_02_085954_create_car_request_images_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarRequestImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('car_request_images', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('image', 50);
+            $table->unsignedBigInteger('request_car_id');
+            $table->foreign('request_car_id')
+                ->references('id')
+                ->on('request_car');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('car_request_images');
+    }
+}
