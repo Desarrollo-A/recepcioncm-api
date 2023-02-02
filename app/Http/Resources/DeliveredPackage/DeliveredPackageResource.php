4c1ca6a96e7d886f8af9e4bd6c584be0fa89ca90
jcoronelg <programador.analista24@ciudadmaderas.com>
2023-02-02 17:31:07 -0600

    Información de paquetería liberada * Se crea resource de la tabla delivered_packages * Se agrega relación en método findByRequestId en PackageRepository.php * Se agrega relación de deliveredPackage en resource de PackageResource.php

---
+++ app/Http/Resources/DeliveredPackage/DeliveredPackageResource.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Resources\DeliveredPackage;
+
+use App\Helpers\Enum\Path;
+use App\Helpers\File;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DeliveredPackageResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        return [
+            'packageId' => $this->package_id,
+            'signature' => File::getExposedPath($this->signature, Path::PACKAGE_SIGNATURES)
+        ];
+    }
+}
